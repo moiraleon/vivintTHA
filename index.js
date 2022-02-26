fd0f63e75a495993a3184fd535bb0a058bb370f4
Moira Leon <moira.leon2@gmail.com>
2022-02-26 09:33:59 -0700

    adding procfile and port

--- index.js
+++ index.js
@@ -20,3 +20,9 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
